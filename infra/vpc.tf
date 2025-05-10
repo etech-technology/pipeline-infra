5f6bda398bc87fff0cf76a3d2494a936b47decc9
etech-technology <165582378+etech-technology@users.noreply.github.com>
2025-05-10 08:13:15 -0400

    Update vpc.tf

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -61,7 +61,7 @@ resource "aws_security_group" "allow_ssh_mysql" {
 
 resource "aws_instance" "web" {
   ami           = "ami-0c55b159cbfafe1f0" # Ubuntu 20.04 LTS in us-east-1
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   subnet_id     = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.allow_ssh_mysql.id]
   key_name      = var.key_pair_name
